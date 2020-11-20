ccb34bf913798718ae5c1f73fe821d86455ef7ec
darauzlova <darauzlova32@gmail.com>
2020-11-20 22:59:37 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function myWindowOpen() {
+  var myWindow = window.open("","","width=250,height=150");
+myWindow.document.write('Название страны происходит от названия германского племени франков, а само слово "франк" означало "свободный". ');
+}
+
+
